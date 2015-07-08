d405ba37d6a0e9d36d454825579c61965662605a
thriqon <github@jonasw.de>
2015-07-08 21:13:48 +0200

    using default config as generated config

--- blueprints/ember-try/files/config/ember-try.js
+++ blueprints/ember-try/files/config/ember-try.js
@@ -1,15 +1,31 @@
 
+'use strict';
+
 module.exports = {
   scenarios: [
-  {
-    name: 'default',
-    dependencies: {}
-  },
-  {
-    name: 'ember-beta',
-    dependencies: {
-      "ember": "beta"
+    {
+      name: "default",
+      dependencies: { } // no dependencies needed as the
+                        // default is already specified in
+                        // the consuming app's bower.json
+    },
+    {
+      name: "ember-release",
+      dependencies: {
+        "ember": "release"
+      }
+    },
+    {
+      name: "ember-beta",
+      dependencies: {
+        "ember": "beta"
+      }
+    },
+    {
+      name: "ember-canary",
+      dependencies: {
+        "ember": "canary"
+      }
     }
-  }
   ]
 };
