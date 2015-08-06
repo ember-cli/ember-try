846ab5eb31c89b2e73a7b1e0534706e83f050f36
Mike North <northm@yahoo-inc.com>
2015-08-05 19:09:41 -0700

    NPM support

--- test/bower-helpers-test.js
+++ test/bower-helpers-test.js
@@ -1,9 +1,21 @@
 var bowerHelpers = require('../lib/utils/bower-helpers');
 var should = require("should");
+var path = require('path');
+var fs = require('fs');
 
 var MockProject = require('./helpers/mock-project');
 
 describe('bowerHelpers', function(){
+  var proj = null;
+  beforeEach(function () {
+    proj = new MockProject('.scratch');
+    return proj.setup();
+  });
+
+  afterEach(function() {
+    return proj.destroy();
+  });
+
   describe('#findBowerPath()', function(){
     it('should return the correct bower path', function(done) {
       bowerHelpers.findBowerPath('.').then(function(path) {
@@ -17,25 +29,41 @@ describe('bowerHelpers', function(){
     // actual bower install could take a while
     this.timeout(10000);
 
-    var proj = new MockProject('.scratch');
-    beforeEach(function () {
-      return proj.setup();
-    });
-
-    afterEach(function() {
-      return proj.destroy();
-    });
-
     it('should replace a bower.json with the bower.json.ember-try', function(done) {
-      var proj = new MockProject('.scratch');
-
       proj.createBowerBackup({
-        'jquery': '2.1.3'
+        dependencies: {
+          jquery: '2.1.3'
+        }
       }).then(function() {
-        bowerHelpers.resetBowerFile(process.cwd() + '/.scratch').then(function () {
+        bowerHelpers.resetBowerFile(proj.projectRoot).then(function () {
           proj.bowerData().then(function(data) {
             data.dependencies.jquery.should.equal('2.1.3');
             done();
+          });
+        });
+      });
+    });
+  });
+
+  describe('#cleanup()', function() {
+    // actual bower install could take a while
+    this.timeout(60000);
+
+
+
+    it('should cleanup the bower situation in a test scenario', function(done) {
+      proj.createBowerBackup({
+        dependencies: {
+          jquery: '2.1.3'
+        }
+      }).then(function() {
+        proj.backupBowerData().then(function(bbd) {
+          bbd.dependencies.jquery.should.equal('2.1.3');
+          bowerHelpers.cleanup(proj.projectRoot).then(function() {
+            proj.bowerData().then(function(bowerData) {
+              bowerData.dependencies.jquery.should.equal('2.1.3');
+              done();
+            });
           });
         });
       });
@@ -46,20 +74,13 @@ describe('bowerHelpers', function(){
     // actual bower install could take a while
     this.timeout(10000);
 
-    var proj = new MockProject('.scratch');
-    beforeEach(function () {
-      return proj.setup();
-    });
-
-    afterEach(function() {
-      return proj.destroy();
-    });
-
     it('should copy bower.json to bower.json.ember-try', function(done) {
       proj.createBowerBackup({
-        'jquery': '2.1.3'
-      }).then(function() {
-        bowerHelpers.backupBowerFile(process.cwd() + '/.scratch').then(function () {
+          dependencies: {
+            'jquery': '2.1.3'
+          }
+        }).then(function() {
+        bowerHelpers.backupBowerFile(proj.projectRoot).then(function () {
           proj.backupBowerData().then(function(bowerData) {
             bowerData.dependencies.jquery.should.equal('^1.11.1');
             done();
@@ -68,5 +89,4 @@ describe('bowerHelpers', function(){
       });
     });
   });
-
-})
+});
--- test/scenario-manager-test.js
+++ test/scenario-manager-test.js
@@ -2,47 +2,145 @@ var ScenarioManager = require('../lib/utils/scenario-manager');
 var should = require("should");
 
 describe('scenarioManager', function() {
-  describe('#_bowerJSONForScenario', function() {
-    it('should change specified bower dependency versions', function() {
+  describe('#_manifestJSONForScenario', function() {
+    it('should change specified bower dependency versions (legacy config schema)', function() {
       var scenarioManager = new ScenarioManager();
-      var bowerJSON = { dependencies: { 'jQuery': '1.11.1' }, resolutions: {} };
-      var scenario =  { dependencies: { 'jQuery': '2.1.3' } };
+      var bowerJSON = {
+        dependencies: {
+          'jQuery': '1.11.1'
+        }
+      };
+      var scenario = {
+        dependencies: {
+          'jQuery': '2.1.3'
+        }
+      };
 
-      bowerJSON = scenarioManager._bowerJSONForScenario(bowerJSON, scenario);
+      var newBowerJSON = scenarioManager
+        ._manifestJSONForScenario(bowerJSON, scenario);
+      newBowerJSON.dependencies['jQuery'].should.equal('2.1.3');
+    });
 
-      bowerJSON.dependencies['jQuery'].should.equal('2.1.3');
+    it('should change specified bower dependency versions (1.0 config schema)', function() {
+      var scenarioManager = new ScenarioManager();
+      var bowerJSON = {
+        dependencies: {
+          jquery: '1.11.1'
+        }
+      };
+      var scenario = {
+        bower: {
+          dependencies: {
+            jquery: '2.1.3'
+          }
+        }
+      };
+
+      var newBowerJSON = scenarioManager
+        ._manifestJSONForScenario(bowerJSON, scenario);
+      newBowerJSON.dependencies['jquery'].should.equal('2.1.3');
+    });
+
+    it('should change favor 1.0 config schema over legacy schema for bower scenario deps', function() {
+      var scenarioManager = new ScenarioManager();
+      var bowerJSON = {
+        dependencies: {
+          jquery: '1.11.1'
+        }
+      };
+      var scenario = {
+        dependencies: {
+          jquery: '~2.0.0'
+        },
+        bower: {
+          dependencies: {
+            jquery: '2.1.3'
+          }
+        }
+      };
+
+      var newBowerJSON = scenarioManager
+        ._manifestJSONForScenario(bowerJSON, scenario);
+      newBowerJSON.dependencies['jquery'].should.equal('2.1.3');
+    });
+
+    it('should respect other root keys in the scenario (i.e., for npm)', function() {
+      var scenarioManager = new ScenarioManager();
+      var packageJSON = {
+        dependencies: {
+          'jQuery': '1.11.1'
+        }
+      };
+      var scenario = {
+        dependencies: {
+          'jQuery': '2.1.3'
+        },
+        npm: {
+          dependencies: {
+            lodash: '^2.0.0'
+          }
+        }
+      };
+
+      var newPackageJSON = scenarioManager
+        ._manifestJSONForScenario(packageJSON, scenario, 'npm');
+      newPackageJSON.dependencies['lodash'].should.equal('^2.0.0');
     });
 
     it('should change specified bower dev dependency versions', function() {
       var scenarioManager = new ScenarioManager();
-      var bowerJSON = { devDependencies: { 'jQuery': '1.11.1' }, resolutions: {} };
-      var scenario =  { devDependencies: { 'jQuery': '2.1.3' } };
+      var bowerJSON = {
+        devDependencies: {
+          'jQuery': '1.11.1'
+        }
+      };
+      var scenario = {
+        devDependencies: {
+          'jQuery': '2.1.3'
+        }
+      };
 
-      bowerJSON = scenarioManager._bowerJSONForScenario(bowerJSON, scenario);
-
+      bowerJSON = scenarioManager
+        ._manifestJSONForScenario(bowerJSON, scenario);
       bowerJSON.devDependencies['jQuery'].should.equal('2.1.3');
     });
 
     it('should add to resolutions', function() {
       var scenarioManager = new ScenarioManager();
-      var bowerJSON = { dependencies: { 'jQuery': '1.11.1' }, resolutions: {} };
-      var scenario =  { dependencies: { 'jQuery': '2.1.3' } };
+      var bowerJSON = {
+        dependencies: {
+          'jQuery': '1.11.1'
+        }
+      };
+      var scenario = {
+        dependencies: {
+          'jQuery': '2.1.3'
+        }
+      };
 
-      bowerJSON = scenarioManager._bowerJSONForScenario(bowerJSON, scenario);
-
+      bowerJSON = scenarioManager
+        ._manifestJSONForScenario(bowerJSON, scenario);
       bowerJSON.resolutions['jQuery'].should.equal('2.1.3');
     });
 
     it('should set custom resolutions', function() {
       var scenarioManager = new ScenarioManager();
-      var bowerJSON = { dependencies: { 'ember': '1.13.5' }, resolutions: {} };
-      var scenario =  {
-        dependencies: { 'ember': 'components/ember#canary' },
-        resolutions:  { 'ember': 'canary' }
+      var bowerJSON = {
+        dependencies: {
+          'ember': '1.13.5'
+        }
+      };
+      var scenario = {
+        dependencies: {
+          'ember': 'components/ember#canary'
+        },
+        resolutions: {
+          'ember': 'canary'
+        }
       };
 
-      bowerJSON = scenarioManager._bowerJSONForScenario(bowerJSON, scenario);
-
+      bowerJSON = scenarioManager
+        ._manifestJSONForScenario(bowerJSON, scenario);
       bowerJSON.resolutions['ember'].should.equal('canary');
     });
   });
