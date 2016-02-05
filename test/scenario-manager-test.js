05ac920aaa7d488649c0933fffaaadc1fbe22e1f
Katie Gengler <katie@kmg.io>
2016-02-05 00:24:23 -0500

    Refactor so that ScenarioManager doesn't need to know about config Add timeout option to running a command

--- test/scenario-manager-test.js
+++ test/scenario-manager-test.js
@@ -4,40 +4,6 @@ var RSVP = require('rsvp');
 var should = require('should');
 
 describe('scenarioManager', function() {
-  describe('new', function() {
-    describe('dependency manager adapter creation', function() {
-      it('creates npm adapter when config has npm key', function() {
-        var manager = new ScenarioManager({ config: { scenarios: [{ npm: {} }]}, project: {root: 'here'}});
-        manager.dependencyManagerAdapters[0].configKey.should.equal('npm');
-        manager.dependencyManagerAdapters.length.should.equal(1);
-      });
-
-      it('creates bower adapter when config has bower key', function() {
-        var manager = new ScenarioManager({ config: { scenarios: [{ bower: {} }]}, project: {root: 'here'}});
-        manager.dependencyManagerAdapters[0].configKey.should.equal('bower');
-        manager.dependencyManagerAdapters.length.should.equal(1);
-      });
-
-      it('creates both adapters when it has both keys', function() {
-        var manager = new ScenarioManager({ config: { scenarios: [{ bower: {}, npm: {}}]}, project: {root: 'here'}});
-        manager.dependencyManagerAdapters[0].configKey.should.equal('npm');
-        manager.dependencyManagerAdapters[1].configKey.should.equal('bower');
-        manager.dependencyManagerAdapters.length.should.equal(2);
-      });
-
-      it('creates bower adapter when legacy dependenies key', function() {
-        var manager = new ScenarioManager({ config: { scenarios: [{ dependencies: {}}]}, project: {root: 'here'}});
-        manager.dependencyManagerAdapters[0].configKey.should.equal('bower');
-        manager.dependencyManagerAdapters.length.should.equal(1);
-      });
-
-      it('creates bower adapter when legacy devDependencies key', function() {
-        var manager = new ScenarioManager({ config: { scenarios: [{ devDependencies: {}}]}, project: {root: 'here'}});
-        manager.dependencyManagerAdapters[0].configKey.should.equal('bower');
-        manager.dependencyManagerAdapters.length.should.equal(1);
-      });
-    });
-  });
 
   describe('#setup', function() {
     it('sets up each of the dependency managers', function() {
