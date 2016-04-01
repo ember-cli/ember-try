b875b62ca68e28b473ccefcdff16bf14c7316aea
Katie Gengler <katie@kmg.io>
2016-04-01 16:02:34 -0700

    Update upload-coverage.sh
    Set token to new Code Climate token

--- upload-coverage.sh
+++ upload-coverage.sh
@@ -1,2 +1,2 @@
 #!/usr/bin/env bash
-CODECLIMATE_REPO_TOKEN=853c1122b5a1a39e5a4938d62f860fa17a11736eb443f6aaaee67ab935b1509b node_modules/.bin/codeclimate-test-reporter < coverage/lcov.info
+CODECLIMATE_REPO_TOKEN=db6ab7e9e6c4f92bda33709f13ab712afc55c3ef7f9c25611c3967e18d552a1a node_modules/.bin/codeclimate-test-reporter < coverage/lcov.info
