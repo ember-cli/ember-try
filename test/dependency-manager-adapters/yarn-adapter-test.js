38dccf99bead96fff32c3aacd7f8b90bcee22da6
Petr Kosikhin <mq3_storm@mail.ru>
2017-02-09 19:47:07 +0300

    fix mocha tests and add mocha tests for yarn. fix lint tests.

--- test/dependency-manager-adapters/yarn-adapter-test.js
+++ test/dependency-manager-adapters/yarn-adapter-test.js
@@ -5,9 +5,11 @@ var RSVP            = require('rsvp');
 var fs              = require('fs-extra');
 var path            = require('path');
 var tmp             = require('tmp-sync');
+var yaml            = require('js-yaml');
 var fixturePackage  = require('../fixtures/package.json');
-var NpmAdapter      = require('../../lib/dependency-manager-adapters/npm');
+var YarnAdapter      = require('../../lib/dependency-manager-adapters/yarn');
 var writeJSONFile   = require('../helpers/write-json-file');
+var writeYamlFile   = require('../helpers/write-yaml-file');
 var generateMockRun = require('../helpers/generate-mock-run');
 
 var remove  = RSVP.denodeify(fs.remove);
@@ -15,7 +17,7 @@ var root    = process.cwd();
 var tmproot = path.join(root, 'tmp');
 var tmpdir;
 
-describe('npmAdapter', function() {
+describe('yarnAdapter', function() {
   beforeEach(function() {
     tmpdir = tmp.in(tmproot);
     process.chdir(tmpdir);
@@ -27,12 +29,14 @@ describe('npmAdapter', function() {
   });
 
   describe('#setup', function() {
-    it('backs up the package.json file and node_modules', function() {
+    it('backs up the package.json, yarn.lock files and node_modules', function() {
       fs.mkdirSync('node_modules');
       writeJSONFile('node_modules/prove-it.json', {originalNodeModules: true});
       writeJSONFile('package.json', {originalPackageJSON: true});
-      return new NpmAdapter({cwd: tmpdir}).setup().then(function() {
+      writeYamlFile('yarn.lock', {originalYarnLock: true});
+      return new YarnAdapter({cwd: tmpdir}).setup().then(function() {
         assertFileContainsJSON('package.json.ember-try', {originalPackageJSON: true});
+        assertYamlFileContainsJSON('yarn.lock.ember-try', {originalYarnLock: true});
         assertFileContainsJSON('.node_modules.ember-try/prove-it.json', {originalNodeModules: true});
       }).catch(function(err) {
         console.log(err);
@@ -42,18 +46,11 @@ describe('npmAdapter', function() {
   });
 
   describe('#_install', function() {
-    it('runs npm prune and npm install', function() {
+    it('runs yarn', function() {
       writeJSONFile('package.json', fixturePackage);
       var runCount = 0;
       var stubbedRun = generateMockRun([{
-        command: 'npm install',
-        callback: function(command, args, opts) {
-          runCount++;
-          expect(opts).to.have.property('cwd', tmpdir);
-          return RSVP.resolve();
-        }
-      }, {
-        command: 'npm prune',
+        command: 'yarn install',
         callback: function(command, args, opts) {
           runCount++;
           expect(opts).to.have.property('cwd', tmpdir);
@@ -61,33 +58,27 @@ describe('npmAdapter', function() {
         }
       }], { allowPassthrough: false });
 
-      return new NpmAdapter({cwd: tmpdir, run: stubbedRun})._install().then(function() {
-        expect(runCount).to.equal(2, 'Both commands should run');
+      return new YarnAdapter({cwd: tmpdir, run: stubbedRun})._install().then(function() {
+        expect(runCount).to.equal(1, 'Runs only yarn install command');
       }).catch(function(err) {
         console.log(err);
         expect(true).to.equal(false, 'Error should not happen');
       });
     });
 
-    it('uses managerOptions for npm commands', function() {
+    it('uses managerOptions for yarn commands', function() {
       writeJSONFile('package.json', fixturePackage);
       var runCount = 0;
       var stubbedRun = generateMockRun([{
-        command: 'npm install --no-shrinkwrap=true',
-        callback: function() {
-          runCount++;
-          return RSVP.resolve();
-        }
-      }, {
-        command: 'npm prune --no-shrinkwrap=true',
+        command: 'yarn install --no-lockfile',
         callback: function() {
           runCount++;
           return RSVP.resolve();
         }
       }], { allowPassthrough: false });
 
-      return new NpmAdapter({cwd: tmpdir, run: stubbedRun, managerOptions: ['--no-shrinkwrap=true']})._install().then(function() {
-        expect(runCount).to.equal(2, 'Both commands should run');
+      return new YarnAdapter({cwd: tmpdir, run: stubbedRun, managerOptions: ['--no-lockfile']})._install().then(function() {
+        expect(runCount).to.equal(1, 'Runs yarn install command with managerOptions');
       }).catch(function(err) {
         console.log(err);
         expect(true).to.equal(false, 'Error should not happen');
@@ -96,13 +87,16 @@ describe('npmAdapter', function() {
   });
 
   describe('#_restore', function() {
-    it('replaces the package.json with the backed up version', function() {
+    it('replaces the package.json and yarn.lock with the backed up versions', function() {
       writeJSONFile('package.json.ember-try', {originalPackageJSON: true});
       writeJSONFile('package.json', {originalPackageJSON: false});
+      writeYamlFile('yarn.lock.ember-try', {originalYarnLock: true});
+      writeYamlFile('yarn.lock', {originalYarnLock: false});
       fs.mkdirSync('.node_modules.ember-try');
       writeJSONFile('.node_modules.ember-try/prove-it.json', {originalNodeModules: true});
-      return new NpmAdapter({cwd: tmpdir})._restore().then(function() {
+      return new YarnAdapter({cwd: tmpdir})._restore().then(function() {
         assertFileContainsJSON('package.json', {originalPackageJSON: true});
+        assertYamlFileContainsJSON('yarn.lock', {originalYarnLock: true});
         assertFileContainsJSON('node_modules/prove-it.json', {originalNodeModules: true});
       }).catch(function(err) {
         console.log(err);
@@ -111,48 +105,52 @@ describe('npmAdapter', function() {
     });
   });
 
-  describe('#_packageJSONForDependencySet', function() {
+  describe('#_newJSONForDependencySet', function() {
     it('changes specified dependency versions', function() {
-      var npmAdapter = new NpmAdapter({cwd: tmpdir});
+      var yarnAdapter = new YarnAdapter({cwd: tmpdir});
       var packageJSON = { devDependencies: { 'ember-feature-flags': '1.0.0' }, dependencies: { 'ember-cli-babel': '5.0.0'} };
       var depSet = { dependencies: { 'ember-cli-babel': '6.0.0' } };
 
-      var resultJSON = npmAdapter._newJSONForDependencySet(packageJSON, depSet);
+      var resultJSON = yarnAdapter._newJSONForDependencySet(packageJSON, depSet);
 
       expect(resultJSON.dependencies['ember-cli-babel']).to.equal('6.0.0');
     });
 
-    it('changes specified npm dev dependency versions', function() {
-      var npmAdapter = new NpmAdapter({cwd: tmpdir});
+    it('changes specified package.json dev dependency versions', function() {
+      var yarnAdapter = new YarnAdapter({cwd: tmpdir});
       var packageJSON = { devDependencies: { 'ember-feature-flags': '1.0.0' }, dependencies: { 'ember-cli-babel': '5.0.0'} };
       var depSet = { devDependencies: { 'ember-feature-flags': '2.0.1' } };
 
-      var resultJSON = npmAdapter._newJSONForDependencySet(packageJSON, depSet);
+      var resultJSON = yarnAdapter._newJSONForDependencySet(packageJSON, depSet);
 
       expect(resultJSON.devDependencies['ember-feature-flags']).to.equal('2.0.1');
     });
 
-    it('changes specified npm peer dependency versions', function() {
-      var npmAdapter = new NpmAdapter({cwd: tmpdir});
+    it('changes specified package.json peer dependency versions', function() {
+      var yarnAdapter = new YarnAdapter({cwd: tmpdir});
       var packageJSON = { peerDependencies: { 'ember-cli-babel': '5.0.0' } };
       var depSet = { peerDependencies: { 'ember-cli-babel': '4.0.0' } };
 
-      var resultJSON = npmAdapter._newJSONForDependencySet(packageJSON, depSet);
+      var resultJSON = yarnAdapter._newJSONForDependencySet(packageJSON, depSet);
 
       expect(resultJSON.peerDependencies['ember-cli-babel']).to.equal('4.0.0');
     });
 
     it('can remove a package', function() {
-      var npmAdapter = new NpmAdapter({cwd: tmpdir});
+      var yarnAdapter = new YarnAdapter({cwd: tmpdir});
       var packageJSON = { devDependencies: { 'ember-feature-flags': '1.0.0' } };
       var depSet = { devDependencies: { 'ember-feature-flags': null } };
 
-      var resultJSON = npmAdapter._newJSONForDependencySet(packageJSON, depSet);
+      var resultJSON = yarnAdapter._newJSONForDependencySet(packageJSON, depSet);
 
       expect(resultJSON.devDependencies).to.not.have.property('ember-feature-flags');
     });
   });
 });
+
+function assertYamlFileContainsJSON(filename, expectedObj) {
+  return assertFileContains(filename, yaml.dump(expectedObj));
+}
 
 function assertFileContainsJSON(filename, expectedObj) {
   return assertFileContains(filename, JSON.stringify(expectedObj, null, 2));
