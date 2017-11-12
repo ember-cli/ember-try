9f10230d2cea34771cc73b2d2d757a8825206a3a
Robert Jackson <rjackson@linkedin.com>
2017-11-12 17:42:40 -0500

    Remove deprecated commands from all-commands.sh.

--- all-commands.sh
+++ all-commands.sh
@@ -31,46 +31,11 @@ ember try:ember '1.13.0' --skip-cleanup=true
 ember try:config
 ember try:config --config-path='test/fixtures/dummy-ember-try-config.js'
 
-# try:testall (Deprecated)
-ember try:testall
-
-# all styles of options for ember-try's own option
-ember try:testall --skip-cleanup
-ember try:testall --skip-cleanup=true
-ember try:testall --skip-cleanup true
-
 # config-path option
 ember try:testall --config-path='test/fixtures/dummy-ember-try-config.js'
 
 # both ember-try options
 ember try:testall --config-path='test/fixtures/dummy-ember-try-config.js' --skip-cleanup true
-
-# try <scenario>  (Deprecated)
-ember try default
-
-# custom command
-ember try default help
-
-# skip-cleanup option
-ember try default --skip-cleanup
-
-# The following two DO NOT CURRENTLY WORK
-# ember try default --skip-cleanup=true
-# ember try default --skip-cleanup true
-
-# config-path option; DOES NOT CURRENTLY WORK
-# ember try test1 --config-path='test/fixtures/dummy-ember-try-config.js'
-
-# both ember-try options; DOES NOT CURRENTLY WORK
-# ember try test1 --config-path='test/fixtures/dummy-ember-try-config.js' --skip-cleanup true
-
-# custom command with all styles of options
-ember try default help --silent
-ember try default help --silent=true
-ember try default help --silent true
-
-# custom command mixed with ember try's own option
-ember try default help --silent --skip-cleanup
 
 # try:one <scenario>
 ember try:one default
@@ -96,7 +61,6 @@ ember try:one default --- ember help --silent true
 
 # custom command mixed with ember try's own option
 ember try:one default --skip-cleanup --- ember help --silent
-
 
 # try:reset
 ember try:reset
