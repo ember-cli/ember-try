244193d55a0d15b3c2512b7a68508b8f53c4a4f6
Robert Jackson <rjackson@linkedin.com>
2017-11-14 16:06:07 -0500

    Do not issue warning when including ember-try in project's package.json.

--- index.js
+++ index.js
@@ -1,20 +1,9 @@
 /* jshint node: true */
 'use strict';
 
-var VersionChecker = require('ember-cli-version-checker');
-
 module.exports = {
   name: 'ember-try',
-  init: function() {
-    this._super.init && this._super.init.apply(this, arguments);
 
-    var checker = new VersionChecker(this);
-    var cliVersion = checker.for('ember-cli', 'npm');
-
-    if (cliVersion.satisfies('>= 2.6.0') && this.project.pkg.devDependencies && this.project.pkg.devDependencies['ember-try']) {
-      this.ui.writeDeprecateLine('ember-try is now included with ember-cli. Including it in your package.json is unnecessary.');
-    }
-  },
   includedCommands: function() {
     return require('./lib/commands');
   }
