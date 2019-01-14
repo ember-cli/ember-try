b1eb2cb45fe0192bf2605dea3bef370f764e2075
Katie Gengler <katie@kmg.io>
2019-01-14 14:44:36 -0500

    Update smoke-test-app

--- smoke-test-app/.eslintrc.js
+++ smoke-test-app/.eslintrc.js
@@ -20,9 +20,13 @@ module.exports = {
     // node files
     {
       files: [
-        'testem.js',
+        '.eslintrc.js',
+        '.template-lintrc.js',
         'ember-cli-build.js',
-        'config/**/*.js'
+        'testem.js',
+        'blueprints/*/index.js',
+        'config/**/*.js',
+        'lib/*/index.js'
       ],
       parserOptions: {
         sourceType: 'script',
@@ -31,15 +35,6 @@ module.exports = {
       env: {
         browser: false,
         node: true
-      }
-    },
-
-    // test files
-    {
-      files: ['tests/**/*.js'],
-      excludedFiles: ['tests/dummy/**/*.js'],
-      env: {
-        embertest: true
       }
     }
   ]
