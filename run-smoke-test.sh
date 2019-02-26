37841fe40d08ea26ac397b51ddef1bf548407831
Chris Manson <chris@manson.ie>
2019-02-26 16:09:13 +0000

    trying without npm pack

--- run-smoke-test.sh
+++ run-smoke-test.sh
@@ -1,8 +1,4 @@
 #!/usr/bin/env bash
 
 set -ex
-rm ember-try*.tgz || true
-npm pack
-now=$(date +%s)
-mv ember-try*.tgz "ember-try-${now}.tgz"
-cd smoke-test-app && npm i && npm i --save-dev "../ember-try-${now}.tgz" && "./${1}"
+cd smoke-test-app && npm i && npm i --save-dev "../" && "./${1}"
