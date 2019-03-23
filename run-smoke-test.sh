c2e7253a5b1bbbfcf94693f142b43a10a7ab0968
Katie Gengler <katie@kmg.io>
2019-03-22 22:18:03 -0700

    try this

--- run-smoke-test.sh
+++ run-smoke-test.sh
@@ -8,6 +8,6 @@ mv ember-try*.tgz "ember-try-${now}.tgz"
 if [[ $npm_execpath =~ ^.*yarn.*$ ]]; then
   cd smoke-test-app && yarn upgrade "ember-try@file:../ember-try-${now}.tgz" && yarn install && "./${1}"
 else
-  cd smoke-test-app && npm install --save-dev "../ember-try-${now}.tgz" && "./${1}"
+  cd smoke-test-app && npm install --save-dev "../ember-try-${now}.tgz" && npm install && "./${1}"
 fi
 
