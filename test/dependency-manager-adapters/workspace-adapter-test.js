31bfb8472264a306a34af7585a7e0c07e6e24b4c
Huy Pham <huyphamily@gmail.com>
2020-02-29 19:19:52 -0800

    Restore backup files after each scenario

--- test/dependency-manager-adapters/workspace-adapter-test.js
+++ test/dependency-manager-adapters/workspace-adapter-test.js
@@ -242,6 +242,77 @@ describe('workspaceAdapter', () => {
     });
   });
 
+  describe('#restoreOriginalDependencies', () => {
+    it('replaces the package.json with the backed up version', () => {
+      fs.ensureDirSync('packages/test/node_modules');
+      writeJSONFile('packages/test/package.json', { originalPackageJSON: false });
+      writeJSONFile('packages/test/node_modules/prove-it.json', { originalNodeModules: false });
+
+      fs.ensureDirSync('packages/test/.node_modules.ember-try');
+      writeJSONFile('packages/test/package.json.ember-try', { originalPackageJSON: true });
+      writeJSONFile('packages/test/.node_modules.ember-try/prove-it.json', {
+        originalNodeModules: true,
+      });
+
+      let workspaceAdapter = new WorkspaceAdapter({
+        cwd: tmpdir,
+        useYarnCommand: true,
+        run: () => Promise.resolve(),
+      });
+
+      return workspaceAdapter.restoreOriginalDependencies().then(() => {
+        assertFileContainsJSON(path.join(tmpdir, 'packages/test/package.json'), {
+          originalPackageJSON: true,
+        });
+        assertFileContainsJSON(path.join(tmpdir, 'packages/test/node_modules/prove-it.json'), {
+          originalNodeModules: true,
+        });
+      });
+    });
+
+    it('replaces the yarn.lock, npm-shrinkwrap.json and package-lock.json with the backed up version if they exist', () => {
+      fs.ensureDirSync('packages/test/node_modules');
+      writeJSONFile('packages/test/package.json', { originalPackageJSON: false });
+      writeJSONFile('packages/test/node_modules/prove-it.json', { originalNodeModules: false });
+      writeJSONFile('packages/test/.yarn.lock', { originalYarnLock: false });
+      writeJSONFile('packages/test/npm-shrinkwrap.json', { originalNpmShrinkWrap: false });
+      writeJSONFile('packages/test/package-lock.json', { originalPackageLock: false });
+
+      fs.ensureDirSync('packages/test/.node_modules.ember-try');
+      writeJSONFile('packages/test/package.json.ember-try', { originalPackageJSON: true });
+      writeJSONFile('packages/test/.node_modules.ember-try/prove-it.json', {
+        originalNodeModules: true,
+      });
+      writeJSONFile('packages/test/yarn.lock.ember-try', { originalYarnLock: true });
+      writeJSONFile('packages/test/npm-shrinkwrap.json.ember-try', { originalNpmShrinkWrap: true });
+      writeJSONFile('packages/test/package-lock.json.ember-try', { originalPackageLock: true });
+
+      let workspaceAdapter = new WorkspaceAdapter({
+        cwd: tmpdir,
+        useYarnCommand: true,
+        run: () => Promise.resolve(),
+      });
+
+      return workspaceAdapter.restoreOriginalDependencies().then(() => {
+        assertFileContainsJSON(path.join(tmpdir, 'packages/test/package.json'), {
+          originalPackageJSON: true,
+        });
+        assertFileContainsJSON(path.join(tmpdir, 'packages/test/node_modules/prove-it.json'), {
+          originalNodeModules: true,
+        });
+        assertFileContainsJSON(path.join(tmpdir, 'packages/test/yarn.lock'), {
+          originalYarnLock: true,
+        });
+        assertFileContainsJSON(path.join(tmpdir, 'packages/test/npm-shrinkwrap.json'), {
+          originalNpmShrinkWrap: true,
+        });
+        assertFileContainsJSON(path.join(tmpdir, 'packages/test/package-lock.json'), {
+          originalPackageLock: true,
+        });
+      });
+    });
+  });
+
   describe('#cleanup', () => {
     it('works without having called #setup first', () => {
       fs.ensureDirSync('packages/test/node_modules');
