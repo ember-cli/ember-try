c2e80dac8e8426b02f59b56ecb40453ccfccf17b
Tobias Bieniek <tobias@bieniek.cloud>
2021-11-04 01:19:16 +0100

    Implement `pnpm` adapter

---
+++ test/dependency-manager-adapters/pnpm-adapter-test.js
@@ -0,0 +1,335 @@
+'use strict';
+
+let expect = require('chai').expect;
+let fs = require('fs-extra');
+let path = require('path');
+let tmp = require('tmp-sync');
+let PnpmAdapter = require('../../lib/dependency-manager-adapters/pnpm');
+let generateMockRun = require('../helpers/generate-mock-run');
+
+let root = process.cwd();
+let tmproot = path.join(root, 'tmp');
+let tmpdir;
+
+describe('pnpm Adapter', () => {
+  beforeEach(() => {
+    tmpdir = tmp.in(tmproot);
+    process.chdir(tmpdir);
+  });
+
+  afterEach(async () => {
+    process.chdir(root);
+    await fs.remove(tmproot);
+  });
+
+  describe('#setup', () => {
+    it('backs up the `package.json` and `pnpm-lock.yaml` files', async () => {
+      await fs.outputJson('package.json', { originalPackageJSON: true });
+      await fs.outputFile('pnpm-lock.yaml', 'originalYAML: true\n');
+      await fs.outputJson('node_modules/prove-it.json', { originalNodeModules: true });
+
+      let adapter = new PnpmAdapter({ cwd: tmpdir });
+      await adapter.setup();
+
+      expect(await fs.readJson('package.json.ember-try')).to.deep.equal({
+        originalPackageJSON: true,
+      });
+      expect(await fs.readFile('pnpm-lock.ember-try.yaml', 'utf-8')).to.equal(
+        'originalYAML: true\n'
+      );
+      expect(fs.existsSync('.node_modules.ember-try')).to.be.false;
+    });
+
+    it('ignores missing `pnpm-lock.yaml` files', async () => {
+      await fs.outputJson('package.json', { originalPackageJSON: true });
+      await fs.outputJson('node_modules/prove-it.json', { originalNodeModules: true });
+
+      let adapter = new PnpmAdapter({ cwd: tmpdir });
+      await adapter.setup();
+
+      expect(await fs.readJson('package.json.ember-try')).to.deep.equal({
+        originalPackageJSON: true,
+      });
+      expect(fs.existsSync('pnpm-lock.ember-try.yaml')).to.be.false;
+      expect(fs.existsSync('.node_modules.ember-try')).to.be.false;
+    });
+  });
+
+  describe('#changeToDependencySet', () => {
+    it('updates the `package.json` and runs `pnpm install`', async () => {
+      await fs.outputJson('package.json', {
+        devDependencies: {
+          'ember-try-test-suite-helper': '0.1.0',
+        },
+      });
+
+      let runCount = 0;
+      let stubbedRun = generateMockRun(
+        [
+          {
+            command: 'pnpm install --frozen-lockfile=false',
+            async callback(command, args, opts) {
+              runCount++;
+              expect(opts).to.have.property('cwd', tmpdir);
+            },
+          },
+        ],
+        { allowPassthrough: false }
+      );
+
+      let adapter = new PnpmAdapter({
+        cwd: tmpdir,
+        run: stubbedRun,
+      });
+
+      await adapter.setup();
+      let result = await adapter.changeToDependencySet({
+        devDependencies: {
+          'ember-try-test-suite-helper': '1.0.0',
+        },
+      });
+
+      expect(result).to.deep.equal([
+        {
+          name: 'ember-try-test-suite-helper',
+          packageManager: 'pnpm',
+          versionExpected: '1.0.0',
+          versionSeen: null,
+        },
+      ]);
+
+      expect(await fs.readJson('package.json')).to.deep.equal({
+        devDependencies: {
+          'ember-try-test-suite-helper': '1.0.0',
+        },
+      });
+
+      expect(await fs.readJson('package.json.ember-try')).to.deep.equal({
+        devDependencies: {
+          'ember-try-test-suite-helper': '0.1.0',
+        },
+      });
+
+      expect(runCount).to.equal(1);
+    });
+  });
+
+  describe('#cleanup', () => {
+    it('restores the `package.json` and `yarn-lock.yaml` files, and then runs `pnpm install`', async () => {
+      await fs.outputJson('package.json', { modifiedPackageJSON: true });
+      await fs.outputJson('package.json.ember-try', { originalPackageJSON: true });
+      await fs.outputFile('pnpm-lock.yaml', 'modifiedYAML: true\n');
+      await fs.outputFile('pnpm-lock.ember-try.yaml', 'originalYAML: true\n');
+
+      let runCount = 0;
+      let stubbedRun = generateMockRun(
+        [
+          {
+            command: 'pnpm install --frozen-lockfile=false',
+            async callback(command, args, opts) {
+              runCount++;
+              expect(opts).to.have.property('cwd', tmpdir);
+            },
+          },
+        ],
+        { allowPassthrough: false }
+      );
+
+      let adapter = new PnpmAdapter({
+        cwd: tmpdir,
+        run: stubbedRun,
+      });
+      await adapter.cleanup();
+
+      expect(await fs.readJson('package.json')).to.deep.equal({ originalPackageJSON: true });
+      expect(await fs.readFile('pnpm-lock.yaml', 'utf-8')).to.equal('originalYAML: true\n');
+      expect(fs.existsSync('package.json.ember-try')).to.be.false;
+      expect(fs.existsSync('pnpm-lock.ember-try.yaml')).to.be.false;
+      expect(fs.existsSync('.node_modules.ember-try')).to.be.false;
+
+      expect(runCount).to.equal(1);
+    });
+  });
+
+  describe('#_packageJSONForDependencySet', () => {
+    it('changes specified dependency versions', () => {
+      let npmAdapter = new PnpmAdapter({ cwd: tmpdir });
+      let packageJSON = {
+        devDependencies: { 'ember-feature-flags': '1.0.0' },
+        dependencies: { 'ember-cli-babel': '5.0.0' },
+      };
+      let depSet = { dependencies: { 'ember-cli-babel': '6.0.0' } };
+
+      let resultJSON = npmAdapter._packageJSONForDependencySet(packageJSON, depSet);
+
+      expect(resultJSON.dependencies['ember-cli-babel']).to.equal('6.0.0');
+    });
+
+    describe('ember property', () => {
+      it('adds the ember property to project package.json', () => {
+        let npmAdapter = new PnpmAdapter({
+          cwd: tmpdir,
+          useYarnCommand: true,
+        });
+        let packageJSON = {};
+        let depSet = {
+          ember: { edition: 'octane' },
+        };
+
+        let resultJSON = npmAdapter._packageJSONForDependencySet(packageJSON, depSet);
+
+        expect(resultJSON).to.deep.equal({ ember: { edition: 'octane' } });
+      });
+
+      it('merges the ember property to project package.json', () => {
+        let npmAdapter = new PnpmAdapter({
+          cwd: tmpdir,
+          useYarnCommand: true,
+        });
+        let packageJSON = { ember: { foo: 'bar' } };
+        let depSet = {
+          ember: { edition: 'octane' },
+        };
+
+        let resultJSON = npmAdapter._packageJSONForDependencySet(packageJSON, depSet);
+
+        expect(resultJSON).to.deep.equal({ ember: { foo: 'bar', edition: 'octane' } });
+      });
+
+      it('overrides existing fields inside the ember property to project package.json', () => {
+        let npmAdapter = new PnpmAdapter({
+          cwd: tmpdir,
+          useYarnCommand: true,
+        });
+        let packageJSON = { ember: { edition: 'classic' } };
+        let depSet = {
+          ember: { edition: 'octane' },
+        };
+
+        let resultJSON = npmAdapter._packageJSONForDependencySet(packageJSON, depSet);
+
+        expect(resultJSON).to.deep.equal({ ember: { edition: 'octane' } });
+      });
+
+      it('removes any items with a null value', () => {
+        let npmAdapter = new PnpmAdapter({
+          cwd: tmpdir,
+          useYarnCommand: true,
+        });
+        let packageJSON = { ember: { edition: 'octane' } };
+        let depSet = {
+          ember: { edition: null },
+        };
+
+        let resultJSON = npmAdapter._packageJSONForDependencySet(packageJSON, depSet);
+
+        expect(resultJSON).to.deep.equal({ ember: {} });
+      });
+    });
+
+    it('adds an override for the specified dependency version', () => {
+      let npmAdapter = new PnpmAdapter({
+        cwd: tmpdir,
+        useYarnCommand: true,
+      });
+      let packageJSON = {
+        dependencies: { 'ember-cli-babel': '5.0.0' },
+      };
+      let depSet = {
+        dependencies: { 'ember-cli-babel': '6.0.0' },
+        overrides: { 'ember-cli-babel': '6.0.0' },
+      };
+
+      let resultJSON = npmAdapter._packageJSONForDependencySet(packageJSON, depSet);
+
+      expect(resultJSON.overrides['ember-cli-babel']).to.equal('6.0.0');
+    });
+
+    it('removes a dependency from overrides if its version is null', () => {
+      let npmAdapter = new PnpmAdapter({
+        cwd: tmpdir,
+        useYarnCommand: true,
+      });
+      let packageJSON = {
+        dependencies: { 'ember-cli-babel': '5.0.0' },
+        overrides: { 'ember-cli-babel': '5.0.0' },
+      };
+      let depSet = {
+        dependencies: { 'ember-cli-babel': '6.0.0' },
+        overrides: { 'ember-cli-babel': null },
+      };
+
+      let resultJSON = npmAdapter._packageJSONForDependencySet(packageJSON, depSet);
+
+      expect(resultJSON.overrides['ember-cli-babel']).to.be.undefined;
+    });
+
+    it('doesnt add resolutions if there are none specified', () => {
+      let npmAdapter = new PnpmAdapter({
+        cwd: tmpdir,
+        useYarnCommand: true,
+      });
+      let packageJSON = {
+        dependencies: { 'ember-cli-babel': '5.0.0' },
+      };
+      let depSet = {
+        dependencies: { 'ember-cli-babel': '6.0.0' },
+      };
+
+      let resultJSON = npmAdapter._packageJSONForDependencySet(packageJSON, depSet);
+
+      expect(resultJSON.resolutions).to.be.undefined;
+    });
+
+    it('doesnt add resolutions when not using yarn', () => {
+      let npmAdapter = new PnpmAdapter({
+        cwd: tmpdir,
+        useYarnCommand: false,
+      });
+      let packageJSON = {
+        dependencies: { 'ember-cli-babel': '5.0.0' },
+      };
+      let depSet = {
+        dependencies: { 'ember-cli-babel': '6.0.0' },
+        resolutions: { 'ember-cli-babel': '6.0.0' },
+      };
+
+      let resultJSON = npmAdapter._packageJSONForDependencySet(packageJSON, depSet);
+
+      expect(resultJSON.resolutions).to.be.undefined;
+    });
+
+    it('changes specified npm dev dependency versions', () => {
+      let npmAdapter = new PnpmAdapter({ cwd: tmpdir });
+      let packageJSON = {
+        devDependencies: { 'ember-feature-flags': '1.0.0' },
+        dependencies: { 'ember-cli-babel': '5.0.0' },
+      };
+      let depSet = { devDependencies: { 'ember-feature-flags': '2.0.1' } };
+
+      let resultJSON = npmAdapter._packageJSONForDependencySet(packageJSON, depSet);
+
+      expect(resultJSON.devDependencies['ember-feature-flags']).to.equal('2.0.1');
+    });
+
+    it('changes specified npm peer dependency versions', () => {
+      let npmAdapter = new PnpmAdapter({ cwd: tmpdir });
+      let packageJSON = { peerDependencies: { 'ember-cli-babel': '5.0.0' } };
+      let depSet = { peerDependencies: { 'ember-cli-babel': '4.0.0' } };
+
+      let resultJSON = npmAdapter._packageJSONForDependencySet(packageJSON, depSet);
+
+      expect(resultJSON.peerDependencies['ember-cli-babel']).to.equal('4.0.0');
+    });
+
+    it('can remove a package', () => {
+      let npmAdapter = new PnpmAdapter({ cwd: tmpdir });
+      let packageJSON = { devDependencies: { 'ember-feature-flags': '1.0.0' } };
+      let depSet = { devDependencies: { 'ember-feature-flags': null } };
+
+      let resultJSON = npmAdapter._packageJSONForDependencySet(packageJSON, depSet);
+
+      expect(resultJSON.devDependencies).to.not.have.property('ember-feature-flags');
+    });
+  });
+});
