7cabca026b582bcfd32e1d10935697f18ca8876a
Chris Manson <chris@manson.ie>
2023-03-23 09:40:41 +0000

    Drop all versions < 3.28 from smoke-test config

--- smoke-test-app/tests/dummy/config/ember-try.js
+++ smoke-test-app/tests/dummy/config/ember-try.js
@@ -7,30 +7,6 @@ module.exports = async function () {
   return {
     scenarios: [
       {
-        name: 'ember-lts-3.16',
-        npm: {
-          devDependencies: {
-            'ember-source': '~3.16.0',
-          },
-        },
-      },
-      {
-        name: 'ember-lts-3.20',
-        npm: {
-          devDependencies: {
-            'ember-source': '~3.20.0',
-          },
-        },
-      },
-      {
-        name: 'ember-lts-3.24',
-        npm: {
-          devDependencies: {
-            'ember-source': '~3.24.3',
-          },
-        },
-      },
-      {
         name: 'ember-lts-3.28',
         npm: {
           devDependencies: {
