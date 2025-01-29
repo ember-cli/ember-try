bda3dce816b2e20312efc473016376a91757b731
Bert De Block <b.deblock@codeshare.be>
2025-01-29 11:04:39 +0100

    Move logic out of `ember-cli` commands

--- index.js
+++ index.js
@@ -4,6 +4,12 @@ module.exports = {
   name: 'ember-try',
 
   includedCommands() {
-    return require('./lib/commands');
+    return {
+      'try:config': require('./lib/ember-cli-commands/config'),
+      'try:each': require('./lib/ember-cli-commands/try-each'),
+      'try:ember': require('./lib/ember-cli-commands/try-ember'),
+      'try:one': require('./lib/ember-cli-commands/try-one'),
+      'try:reset': require('./lib/ember-cli-commands/reset'),
+    };
   },
 };
